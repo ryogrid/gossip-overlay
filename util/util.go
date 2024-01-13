074dac201086b4e1c582c5efb2c265fa71e9edd5
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-01-13 22:55:40 +0900

    debugging: WIP.

--- util/util.go
+++ util/util.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
 	"fmt"
@@ -62,7 +61,8 @@ func OverlayDebugPrintln(a ...interface{}) {
 func NewHashIDUint64(key string) uint64 {
 	hf := sha256.New()
 	hf.Write([]byte(key))
-	var ret uint64
-	binary.Read(bytes.NewBuffer(hf.Sum(nil)[0:7]), binary.LittleEndian, &ret)
-	return ret
+	//var ret uint64
+	//binary.Read(bytes.NewBuffer(hf.Sum(nil)[0:7]), binary.LittleEndian, &ret)
+	//return ret
+	return binary.LittleEndian.Uint64(hf.Sum(nil))
 }
