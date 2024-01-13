424f7dd33ed3bf1721a71a0893a10a5ab147b935
Ryo Kanbayashi <ryo.contact@gmail.com>
2024-01-13 22:26:30 +0900

    implemented OverlayListener.

--- overlay/overlay_listener.go
+++ overlay/overlay_listener.go
@@ -1,30 +1,44 @@
 package overlay
 
-import "net"
+import (
+	"github.com/ryogrid/gossip-overlay/gossip"
+	"net"
+)
 
 type OverlayListener struct {
+	overlayPeer   *OverlayPeer
+	overlayServer *OverlayServer
 }
 
 func NewOverlayListener(ol *OverlayPeer) net.Listener {
-	// TODO: need to implement (private_server.go::getGossipListener)
-	return &OverlayListener{}
+	oserv, err := NewOverlayServer(ol.Peer, ol.Peer.GossipMM)
+	if err != nil {
+		panic(err)
+	}
+
+	return &OverlayListener{
+		overlayPeer:   ol,
+		overlayServer: oserv,
+	}
 }
 
 // Accept waits for and returns the next connection to the listener.
 func (ol *OverlayListener) Accept() (net.Conn, error) {
-	// TODO: need to implement (OverlayListerner::Accept)
-	panic("not implemented")
+	channel, _, _, err := ol.overlayServer.Accept()
+	return channel, err
 }
 
 // Close closes the listener.
 // Any blocked Accept operations will be unblocked and return errors.
 func (ol *OverlayListener) Close() error {
-	// TODO: need to implement (OverlayListerner::Close)
-	panic("not implemented")
+	// TODO: need to implement (OverlayListener::Close)
+	// do nothing now
+	return nil
 }
 
 // Addr returns the listener's network address.
 func (ol *OverlayListener) Addr() net.Addr {
-	// TODO: need to implement (OverlayListerner::Addr)
-	panic("not implemented")
+	return &gossip.PeerAddress{
+		PeerName: ol.overlayPeer.Peer.GossipDataMan.Self,
+	}
 }
